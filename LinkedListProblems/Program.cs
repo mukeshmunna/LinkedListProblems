391b4cc9ecd9f483eea54bbbac58735857e61945
Mukkara Mukesh <mukkara.m@tvsnext.com>
2023-07-27 12:23:52 +0530

    [Mukesh] add : created for queue feature

--- LinkedListProblems/Program.cs
+++ LinkedListProblems/Program.cs
@@ -5,25 +5,14 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("LinkedList Problem");
-        Console.WriteLine("Welcome to the Linked List Operation");
-        LinkedList list = new LinkedList();
-        list.Add(56);
-        list.Add(30);
-        list.Add(70);
-        /* list.AddInReverseOrder(50);
-         list.AddInReverseOrder(60);
-         list.AddInReverseOrder(70); */
-        list.InserAtParticularPosition(1, 100);
-        // list.RemoveFirstNode();
-        //list.RemoveLastNode();
-        int num = list.Search(30);
-        Console.WriteLine("The position is " + num);
-        list.DeleteNodeAtParticularPosition(1);
-
-
-
-        list.Display();
+        Console.WriteLine("Queue Fuction");
+        LinKedListQueue queue = new LinKedListQueue();
+        queue.Enqueue(56);
+        queue.Enqueue(30);
+        queue.Enqueue(70);
+        queue.Display();
+        queue.Dequeue();
+        queue.Display();
 
     }
 }
