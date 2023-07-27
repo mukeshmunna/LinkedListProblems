a62a4fc39aeca0b7e9bf638cc0bda76c236a5de3
Mukkara Mukesh <mukkara.m@tvsnext.com>
2023-07-27 10:52:10 +0530

    [Mukesh] add: added a new feature for Delete first node

--- LinkedListProblems/Program.cs
+++ LinkedListProblems/Program.cs
@@ -6,7 +6,7 @@ public class Program
     public static void Main(string[] args)
     {
         Console.WriteLine("LinkedList Problem");
-        Console.WriteLine("Linked List Operation");
+        Console.WriteLine("Welcome to the Linked List Operation");
         LinkedList list = new LinkedList();
         list.Add(56);
         list.Add(30);
@@ -14,7 +14,8 @@ public class Program
         /* list.AddInReverseOrder(50);
          list.AddInReverseOrder(60);
          list.AddInReverseOrder(70); */
-        list.InserAtParticularPosition(3, 60);
+        list.InserAtParticularPosition(10, 100);
+        list.RemoveFirstNode();
 
         list.Display();
 
